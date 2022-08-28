8e211e1a43b406b2b7d93549257df79a18888dc4
mazza <mazzapaolo2019@gmail.com>
2022-08-28 11:29:21 +0100

    some very good work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,35 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { Camera } from '@mediapipe/camera_utils';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands';
+import * as FileSaver from 'file-saver';
 import * as _ from 'lodash';
+import { QuillEditorComponent } from 'ngx-quill';
+
 
 import * as ort from 'onnxruntime-web'
-import * as jup from "@jupyterlite/server"
 const gesture_mapping: { [name: number]: string } = {
   0: "other",
   1: "thumb up",
   2: "thumb down"
 }
 
-let current_gesture = ""
 
+let current_gesture = ""
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
-
 export class AppComponent implements AfterViewInit {
+  @ViewChild(QuillEditorComponent) editor?: QuillEditorComponent;
   private camera!: Camera;
   hands: Hands;
   title = 'multimodal-text-editor';
   _gesture: string = "";
+  curret_text: string = "";
+  current_action_count: number = 0
 
   public get gesture() {
     return this._gesture;
@@ -46,7 +48,7 @@ export class AppComponent implements AfterViewInit {
     });
     this.hands.setOptions({ minDetectionConfidence: 0.5, maxNumHands: 1, modelComplexity: 1 })
     this.gesture = ""
-    new jup.JupyterLiteServer({})
+    localStorage.setItem("html", "<h1>Hello World!</h1>")
   }
 
 
@@ -78,22 +80,63 @@ export class AppComponent implements AfterViewInit {
         drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 1 });
       }
       current_gesture = gesture_mapping[pred]
+    } else {
+      current_gesture = ""
     }
     canvasCtx.restore();
   }
 
 
+  created(event: any) {
+    console.log(event);
+    var html = localStorage.getItem('html');
+    if (html != null) {
+      event.root.innerHTML = html;
+    }
+  }
 
+  contentChanged(obj: any) {
+    localStorage.setItem('html', obj.html);
+    this.curret_text = obj.html
+  }
+
+  actionToTake(event: string) {
+    if (event === "other") {
+      return
+    } else if (event === "thumb up") {
+      this.upload()
+    } else if (event === "thumb down") {
+      this.download()
+    }
+  }
+
+  download() {
+    var blob = new Blob(["Hello, world!"], { type: "text/plain;charset=utf-8" });
+    FileSaver.saveAs(blob, "hello world.txt");
+  }
+
+  upload() {
+    this.curret_text += "<b>SAY WHAT!?!</b>"
+  }
 
   ngAfterViewInit(): void {
     let video = document.querySelector("#hello") as HTMLVideoElement;
-
     this.hands.onResults(this.onResults);
 
     this.camera = new Camera(video, {
       onFrame: async () => {
         await this.hands.send({ image: video });
+        if (current_gesture !== "" && this.gesture === current_gesture) {
+          this.current_action_count += 1
+        } else {
+          this.current_action_count = 1
+        }
         this.gesture = current_gesture
+
+        if (this.current_action_count == 20) {
+          this.actionToTake(this.gesture)
+          this.current_action_count = 1
+        }
       },
       width: 1280,
       height: 720
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot(),
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
